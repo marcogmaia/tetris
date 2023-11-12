a0583d8b4ed1c60c778b8a28678183da5bb7424d
Marco Maia <marco.maia@iarasystems.com.br>
2023-11-12 13:04:41 -0300

    [skip-ci] Add clock tick class

--- src/maia/tetris/shape.h
+++ src/maia/tetris/shape.h
@@ -2,8 +2,8 @@
 
 #pragma once
 
+#include <array>
 #include <numbers>
-#include <vector>
 
 #include <raylib.h>
 
@@ -16,15 +16,52 @@ using Position = Vector2<int>;
 struct Shape {
   Color color;
   // Occupied positions by the shape.
-  std::vector<Position> positions;
+  std::array<Position, 4> positions;
   // Check if the shape can be moved.
   bool frozen{};
+};
+
+struct Tetrominos {
+  // https://www.wikiwand.com/en/Tetromino
+  static constexpr std::array<Shape, 5> kTetrominos{
+      Shape{  .color{BLUE}, .positions{Position{0, 0}, Position{1, 0}, Position{2, 0}, Position{3, 0}}, .frozen{}},
+      Shape{.color{YELLOW}, .positions{Position{0, 0}, Position{0, 1}, Position{1, 0}, Position{1, 1}}, .frozen{}},
+      Shape{  .color{PINK}, .positions{Position{0, 1}, Position{1, 1}, Position{2, 1}, Position{1, 0}}, .frozen{}},
+      Shape{.color{ORANGE}, .positions{Position{0, 0}, Position{0, 1}, Position{0, 2}, Position{1, 0}}, .frozen{}},
+      Shape{ .color{GREEN}, .positions{Position{0, 0}, Position{1, 0}, Position{1, 1}, Position{2, 1}}, .frozen{}},
+  };
+
+  enum class Tetromino {
+    kStraight = 0,
+    kSquare,
+    kT,
+    kL,
+    kSkew,
+  };
+
+  static constexpr Shape Get(Tetromino tetromino) {
+    static_assert(kTetrominos.size() - 1 == static_cast<size_t>(Tetromino::kSkew));
+    return kTetrominos[static_cast<int>(tetromino)];
+  }
+
+  static Shape GetRandom() {
+    return kTetrominos[GetRandomValue(0, kTetrominos.size() - 1)];
+  }
 };
 
 inline void Move(int dx, int dy, Shape &shape) {
   for (auto &pos : shape.positions) {
     pos.x += dx;
     pos.y -= dy;
+  }
+}
+
+inline void SetPosition(int x, int y, Shape &shape) {
+  // const auto max = std::max_element(
+  //     shape.positions.cbegin(), shape.positions.cend(), [](Position pos1, Position pos2) { return pos1.y > pos2.y;
+  //     });
+  for (auto &pos : shape.positions) {
+    pos = pos + Position{x, y};
   }
 }
 
