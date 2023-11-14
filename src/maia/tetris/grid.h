4d58dbb99d5d029b75f03bcab47e7a4da40c18b9
Marco Maia <marco.maia@iarasystems.com.br>
2023-11-14 03:24:48 -0300

    [skip-ci] Some refactoring

--- src/maia/tetris/grid.h
+++ src/maia/tetris/grid.h
@@ -2,6 +2,7 @@
 
 #pragma once
 
+#include <span>
 #include <vector>
 
 #include <raylib.h>
@@ -49,7 +50,7 @@ class Grid {
 
   void FillWith(const Shape &shape) {
     for (const auto &pos : shape.positions) {
-      if (auto block = at(pos.x, pos.y)) {
+      if (auto block = at(std::lroundf(pos.x), std::lroundf(pos.y))) {
         block->get() = Block{
             .color = shape.color,
             .filled = true,
@@ -66,6 +67,10 @@ class Grid {
     return grid_;
   }
 
+  std::vector<std::span<Block>> rows() {
+    return rows_;
+  }
+
  private:
   static int GetIndex(int x, int y) {
     return x + y * kWidth;
@@ -75,10 +80,33 @@ class Grid {
     return 0 <= index && index < grid_.size();
   }
 
+  std::span<Block> GetGridRow(int index) {
+    const auto begin_row = std::next(grid_.begin(), index * Grid::GridWidth());
+    const auto end_row = std::next(begin_row, Grid::GridWidth());
+    return std::span<Block>(begin_row, end_row);
+  }
+
+  static std::vector<std::span<Block>> InitRows(std::vector<Block> &grid) {
+    const auto get_grid_row = [&grid](int index) -> std::span<Block> {
+      const auto begin_row = std::next(grid.begin(), index * Grid::GridWidth());
+      const auto end_row = std::next(begin_row, Grid::GridWidth());
+      return std::span<Block>(begin_row, end_row);
+    };
+
+    std::vector<std::span<Block>> rows;
+    rows.reserve(Grid::GridHeight());
+    for (int row = 0; row < Grid::GridHeight(); ++row) {
+      rows.emplace_back(get_grid_row(row));
+    }
+    return rows;
+  }
+
   static constexpr int kWidth = 10;
   static constexpr int kHeight = 20;
   //  This is stored in row major order.
   std::vector<Block> grid_ = std::vector<Block>(kWidth * kHeight, Block{});
+  std::vector<std::span<Block>> rows_ = InitRows(grid_);
+  ;
 };
 
 inline void DrawGrid(const Grid &grid) {
