34a36680f024350e59f6c024dae749ab0ae3722d
Marco Maia <marco.maia@iarasystems.com.br>
2023-11-14 01:38:01 -0300

    [skip-ci] Add remove filled rows

--- src/maia/tetris/shape_test.cpp
+++ src/maia/tetris/shape_test.cpp
@@ -9,22 +9,33 @@
 namespace maia {
 
 TEST(Shape, Rotate) {
-  constexpr int kUnit = 100;
   std::vector<Position> expecteds{
-      {     0,  kUnit},
-      {-kUnit,      0},
-      {     0, -kUnit},
-      { kUnit,      0}
+      { 0,  1},
+      {-1,  0},
+      { 0, -1},
+      { 1,  0}
   };
-  for (Position pos{kUnit, 0}; auto &expected : expecteds) {
+  for (Position pos{1, 0}; auto &expected : expecteds) {
     pos = Rotate(pos, 90);
-    EXPECT_EQ(pos.x, expected.x);
-    EXPECT_EQ(pos.y, expected.y);
+    EXPECT_NEAR(pos.x, expected.x, 1e-6);
+    EXPECT_NEAR(pos.y, expected.y, 1e-6);
   }
 }
 
 TEST(Shape, RotateStraight) {
   auto shape = Tetrominos::Get(Tetrominos::Tetromino::kStraight);
+  auto rotated_shape = RotateShapeLeft(shape);
+  auto expected_positions = std::vector<Position>{
+      {1, -2},
+      {1, -1},
+      {1,  0},
+      {1,  1}
+  };
+  ASSERT_EQ(expected_positions.size(), rotated_shape.positions.size());
+  for (int i = 0; i < expected_positions.size(); ++i) {
+    EXPECT_NEAR(expected_positions[i].x, rotated_shape.positions[i].x, 1e-6);
+    EXPECT_NEAR(expected_positions[i].y, rotated_shape.positions[i].y, 1e-6);
+  }
 }
 
 }  // namespace maia
